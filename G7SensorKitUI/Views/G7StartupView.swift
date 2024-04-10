1ab82de1bcceb4e4bbb33d7945c5d22eeca2a12d
marionbarker <marionbarker@earthlink.net>
2024-04-10 05:30:01 -0700

    modify text with appName so translation string remains constant

--- G7SensorKitUI/Views/G7StartupView.swift
+++ G7SensorKitUI/Views/G7StartupView.swift
@@ -28,7 +28,7 @@ struct G7StartupView: View {
                     .frame(height: 120)
                     .padding(.horizontal)
             }.frame(maxWidth: .infinity)
-            Text(LocalizedString("\(self.appName) can read G7 CGM data, but you must still use the Dexcom G7 App for pairing, calibration, and other sensor management.", comment: "Descriptive text on G7StartupView"))
+            Text(String(format: LocalizedString("%1$@ can read G7 CGM data, but you must still use the Dexcom G7 App for pairing, calibration, and other sensor management.", comment: "Descriptive text on G7StartupView  (1: appName)"), self.appName))
                 .fixedSize(horizontal: false, vertical: true)
                 .foregroundColor(.secondary)
             Spacer()
